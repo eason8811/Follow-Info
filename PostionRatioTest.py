c335d41ce6f0a4ede2c0bbd1be5c4d16e5569d95
eason8811 <3220563569@qq.com>
2024-08-18 22:32:03 +0800

    尝试使用币安的api

--- PostionRatioTest.py
+++ PostionRatioTest.py
@@ -21,16 +21,18 @@ secret_key = 'mwU7KCworFZ17WIOqRuGaRmtwT3nnUDBhtg8HQf9CHFB7KVSxev0Rwym5mgfWjDx'
 u_api = BinanceAPI(base_url, api_key, secret_key)
 
 # 爬取参数
-kline_amount = 2880
+kline_amount = 2800
 period = 15  # 分钟
 
 top_position_ratio = []
 top_account_ratio = []
 amount = kline_amount
+end_time = int(time.time()) * 1000
 while amount > 0:
     body = {
         'symbol': 'BTCUSDT',
-        'period	': '15m',
+        'period': '15m',
+        'endTime': end_time,#1723541400000
         'limit': min(500, amount)
     }
     respond = u_api.api('GET', '/futures/data/topLongShortPositionRatio', body)
@@ -45,8 +47,9 @@ while amount > 0:
         respond[i]['longAccount'] = float(respond[i]['longAccount'])
         respond[i]['shortAccount'] = float(respond[i]['shortAccount'])
     top_account_ratio.extend(respond)
-    print(f'\r{round((2880 - amount) / 2880, 5) * 100}%', end='')
+    print(f'\r{round((2880 - amount) / 2880 * 100, 3)}%', end='')
     amount -= 500
+    end_time -= period * 60 * 1000 * 500
 print('\r大数据获取完成')
 
 klines = []
@@ -69,9 +72,9 @@ while amount > 0:
             'close': float(item[4]),
         }
         klines.append(kline)
-    print(f'\r{round((2880 - amount) / 2880, 5) * 100}%', end='')
+    print(f'\r{round((2880 - amount) / 2880 * 100, 3)}%', end='')
     amount -= 1500
-    end_time -= period * 60 * 1000
+    end_time -= period * 60 * 1000 * 1500
 print('\rK线数据获取完成')
 
 close_data = normalize(list(map(lambda x: x['close'], klines)))  # 收盘价
@@ -82,13 +85,14 @@ long_short_account_ratio = normalize(list(map(lambda x: x['longShortRatio'], top
 long_account = normalize(list(map(lambda x: x['longAccount'], top_position_ratio)))  # 大户持的多仓账户数占全市场的比值
 short_account = normalize(list(map(lambda x: x['shortAccount'], top_position_ratio)))  # 大户持的空仓账户数占全市场的比值
 
-plt.plot(list(map(lambda x: x['time'], close_data)), close_data, label='close')
-plt.plot(list(map(lambda x: x['time'], close_data)), long_short_position_ratio, label='long_short_position_ratio')
-# plt.plot(list(map(lambda x: x['time'], close_data)), long_position, label='long_position')
-# plt.plot(list(map(lambda x: x['time'], close_data)), short_position, label='short_position')
-# plt.plot(list(map(lambda x: x['time'], close_data)), long_short_account_ratio, label='long_short_account_ratio')
-# plt.plot(list(map(lambda x: x['time'], close_data)), long_account, label='long_account')
-# plt.plot(list(map(lambda x: x['time'], close_data)), short_account, label='short_account')
+plt.plot(list(map(lambda x: x['time'] / 1000, klines)), close_data, label='close')
+plt.plot(list(map(lambda x: x['time'] / 1000, klines)), list(long_short_position_ratio),
+         label='long_short_position_ratio')
+# plt.plot(list(map(lambda x: x['time']/1000, klines)), long_position, label='long_position')
+# plt.plot(list(map(lambda x: x['time']/1000, klines)), short_position, label='short_position')
+# plt.plot(list(map(lambda x: x['time']/1000, klines)), long_short_account_ratio, label='long_short_account_ratio')
+# plt.plot(list(map(lambda x: x['time']/1000, klines)), long_account, label='long_account')
+# plt.plot(list(map(lambda x: x['time']/1000, klines)), short_account, label='short_account')
 
 plt.legend()
 plt.ticklabel_format(useOffset=False, style='plain')
